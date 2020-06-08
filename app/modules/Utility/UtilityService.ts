eed4c069c5ec123b280d09844e5aa08ea056a196
rubensPC <rubendroogh@hotmail.nl>
2020-06-08 18:25:49 +0200

    Created RandomHexColor utility function.

--- app/modules/Utility/UtilityService.ts
+++ app/modules/Utility/UtilityService.ts
@@ -5,6 +5,16 @@ export default abstract class UtilityService {
         utils.OpenUrl(url);
     }
 
+    public static getRandomHexColor(): string {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+      
+
     public static log(message: any, prefix?: any): void {
         var callerName = prefix ?? this.log.caller.name
         console.log(`[${callerName}]: ${message}`)
